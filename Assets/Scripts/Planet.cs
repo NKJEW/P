db9b2bf9c119212daafed44121e98a40dc694e1d
Nathaniel Jew <nathankjew@gmail.com>
2018-12-01 19:22:17 -0800

    Added smooth planet shrinking with temporary curve

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Planet : MonoBehaviour {
+    [Header("Planet")]
     public Transform mainPlanet;
     public Transform core;
 
-    [Space(15)]
+    [Header("Shrinking")]
+    public AnimationCurve shrinkCurve;
+    public float shrinkTime;
+    IEnumerator curShrinkSequence;
+
+    [Header("Stats")]
     public float initialRadius;
     public float coreRadius;
     public float maxHealth;
@@ -22,9 +28,9 @@ public class Planet : MonoBehaviour {
         curHealth = maxHealth;
 
         core.transform.localScale = new Vector3(coreRadius * 2, coreRadius * 2, 1);
+        mainPlanet.transform.localScale = new Vector3(initialRadius * 2, initialRadius * 2, 1);
 
         UpdateRadius();
-
         SpawnTerrain();
     }
 
@@ -46,12 +52,36 @@ public class Planet : MonoBehaviour {
     }
 
     void UpdateRadius() {
+        if (curShrinkSequence != null) {
+            StopCoroutine(curShrinkSequence);
+        }
+        curShrinkSequence = ShrinkSequence();
+        StartCoroutine(curShrinkSequence);
+    }
+
+    IEnumerator ShrinkSequence() {
+        float startRadius = mainPlanet.localScale.x / 2;
+
         float healthPercent = curHealth / maxHealth;
-        radius = coreRadius + healthPercent * (initialRadius - coreRadius);
+        float endRadius = coreRadius + healthPercent * (initialRadius - coreRadius);
+        float p = 0f;
 
-        mainPlanet.transform.localScale = new Vector3(radius * 2, radius * 2, 1);
-        col.radius = radius;
+        col.radius = endRadius;
 
+        while (p < 1f) {
+            radius = Mathf.LerpUnclamped(startRadius, endRadius, shrinkCurve.Evaluate(p));
+            UpdatePlanet();
+            yield return new WaitForEndOfFrame();
+            p += Time.deltaTime / shrinkTime;
+        }
+
+        radius = endRadius;
+        UpdatePlanet();
+        curShrinkSequence = null;
+    }
+
+    void UpdatePlanet() {
+        mainPlanet.transform.localScale = new Vector3(radius * 2, radius * 2, 1f);
         UpdateTerrain();
         Player.instance.UpdateRadius();
     }
