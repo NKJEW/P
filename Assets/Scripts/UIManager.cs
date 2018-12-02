02133c2156e9afbe0241bced81d5e643842daa18
Nathaniel Jew <nathankjew@gmail.com>
2018-12-01 20:14:48 -0800

    Temporary indicator stuff (not functional)

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+    public static UIManager instance;
+
+    public GameObject indicatorPrefab;
+    public Canvas canvas;
+
+    void Awake() {
+        instance = this;
+    }
+
+    public Indicator CreateIndicator(GameObject _newTarget) {
+        GameObject edgeViewObj = Instantiate(indicatorPrefab, canvas.transform);
+        Indicator newIndicator = edgeViewObj.GetComponent<Indicator>();
+        newIndicator.Init(canvas);
+        newIndicator.SetTarget(_newTarget);
+        return newIndicator;
+    }
+}
